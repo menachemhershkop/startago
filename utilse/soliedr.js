272f03e109821163e1ddc5f319c99860d2ea2ad3
yosef morad <yos0556773004@gmail.com>
2025-12-07 17:01:16 +0200

    ff

---
+++ utilse/soliedr.js
@@ -0,0 +1,9 @@
+function craeteSoldier(){
+    let soldiers = []
+    for (let i = 1 ; i < 11 ; i++){
+        let soldier = {name:'soldier',rank :i} 
+        soldiers.push(soldier)
+    }soldiers.push({name:"flag" ,rank:null})
+    return soldiers
+}
+console.log(craeteSoldier());
